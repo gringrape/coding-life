ae7918a92bc45c2285b3553bdbc3b1b54c30d1ed
gringrape <gringrape200@gmail.com>
2022-07-17 21:35:37 +0900

    가장 단순한 예제

--- 20220717/useSyncExternalStore/src/App.tsx
+++ 20220717/useSyncExternalStore/src/App.tsx
@@ -1,5 +1,42 @@
+import { useSyncExternalStore } from 'react';
+
+const store = {
+  state: { count: 0 },
+  listeners: new Set(),
+  getSnapshot() {
+    return this.state;
+  },
+  subcribe(callback: any) {
+    this.listeners.add(callback);
+    return () => this.listeners.delete(callback);
+  },
+  setState(reducer) {
+    this.state = {
+      ...this.state,
+      ...reducer(this.state),
+    };
+    this.listeners.forEach((f) => f());
+  },
+};
+
 export default function App() {
+  const { count } = useSyncExternalStore(
+    store.subcribe.bind(store),
+    store.getSnapshot.bind(store),
+  );
+
+  const handleClick = () => {
+    store.setState((state) => ({
+      ...state,
+      count: count + 1,
+    }));
+  };
+
   return (
-    <h1>hello, world</h1>
+    <>
+      <h1>hello, world</h1>
+      <p>{count}</p>
+      <button type="button" onClick={handleClick}>+</button>
+    </>
   );
 }
