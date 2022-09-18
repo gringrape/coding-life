3988796a4a7e542d32f5a7ee2053e28a9c0f3181
gringrape <gringrape200@gmail.com>
2022-09-18 16:19:14 +0900

    Setup a new gradle project
    Install dependencies
    - AssertJ: https://assertj.github.io/doc/

---
+++ 20220918/tddbe/app/src/test/java/tddbe/MoneyTest.java
@@ -0,0 +1,11 @@
+package tddbe;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MoneyTest {
+    @Test void simple() {
+        assertThat(1 + 1).isEqualTo(2);
+    }
+}
