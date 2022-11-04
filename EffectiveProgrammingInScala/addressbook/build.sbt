6aacdd52996ba23f2fbd57554c9c3e9d01da7cf9
진 <jin@jin-ui-MacBookPro.local>
2022-11-04 16:48:43 +0900

    Effective Programming in Scala, AddressBook 예제
    List로 모델링할 수 있는 구조를 생성하고 조작하는 연습.

--- EffectiveProgrammingInScala/addressbook/build.sbt
+++ EffectiveProgrammingInScala/addressbook/build.sbt
@@ -1,10 +1,11 @@
-lazy val root = (project in file(".")).
-  settings(
-    inThisBuild(List(
+lazy val root = (project in file(".")).settings(
+  inThisBuild(
+    List(
       organization := "com.example",
-      scalaVersion := "2.13.6"
-    )),
-    name := "scalatest-example"
-  )
+      scalaVersion := "3.2.0"
+    )
+  ),
+  name := "scalatest-example"
+)
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.9" % Test
--- EffectiveProgrammingInScala/addressbook/src/test/scala/AddressBookTest.scala
+++ EffectiveProgrammingInScala/addressbook/src/test/scala/AddressBookTest.scala
@@ -1,7 +1,60 @@
-import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.funspec.AnyFunSpec
 
-class CubeCalculatorTest extends AnyFunSuite {
-  test("simple") {
-    assert(1 + 1 === 2)
+case class Contact(name: String, phoneNumbers: List[String])
+
+case class AddressBook(contacts: List[Contact]):
+  val numberOfContacts = contacts.size
+
+  val contactNames = contacts.map(contact => contact.name)
+
+  val contactsWithPhone =
+    contacts.filter(contact => contact.phoneNumbers.nonEmpty)
+
+  def addContact(contact: Contact) =
+    this.copy(contacts = contacts.appended(contact))
+
+class CubeCalculatorTest extends AnyFunSpec:
+  describe("AddressBook") {
+    it("created without crash") {
+      val alice = Contact(name = "Alice", phoneNumbers = List(""))
+      val bob = Contact(name = "Bob", phoneNumbers = List(""))
+
+      AddressBook(contacts = List(alice, bob))
+    }
+
+    describe("addContact") {
+      it("increases number of contacts") {
+        val addressBook = AddressBook(contacts = List())
+
+        assert(addressBook.numberOfContacts === 0)
+
+        val alice = Contact(name = "Alice", phoneNumbers = List(""))
+
+        assert(addressBook.addContact(alice).numberOfContacts === 1)
+      }
+    }
+
+    describe("contactNames") {
+      it("get names in contacts") {
+        val alice = Contact(name = "Alice", phoneNumbers = List(""))
+
+        val addressBook = AddressBook(contacts = List(alice))
+
+        val names = addressBook.contactNames
+        assert(names(0) == alice.name)
+      }
+    }
+
+    describe("contactsWithPhone") {
+      it("filters contacts with phone number") {
+        val alice = Contact(name = "Alice", phoneNumbers = List(""))
+        val bob = Contact(name = "Bob", phoneNumbers = List("123-1234"))
+        val jason = Contact(name = "Jason", phoneNumbers = List("456-4567"))
+
+        val addressBook = AddressBook(contacts = List(alice, bob))
+
+        val contacts = addressBook.contactsWithPhone
+        assert(contacts.size == 2)
+      }
+    }
   }
-}
