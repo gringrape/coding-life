43c5fc079d5ae972f2b39f998dc02a6594d95239
gringrape <gringrape200@gmail.com>
2022-11-22 16:28:14 +0900

    SICP 연습문제 1-32 풀이

--- SICP/1-3/ex-1-32.rkt
+++ SICP/1-3/ex-1-32.rkt
@@ -21,3 +21,14 @@
 
 (product 1 5) ; 120
 (product 4 7) ; 840
+
+(define (factorial n)
+  (accumulate-iter * 1 identity 1 inc n))
+
+(define (accumulate-iter combiner null-value term a next b)
+  (define (iter n result)
+    (if (> n b) result (iter (next n) (combiner result (term n)))))
+  (iter a null-value))
+
+(factorial 5) ; 120
+(factorial 6) ; 720
