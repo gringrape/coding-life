5376ad247f42e5d200c5d50cd61c1df206fe6ddf
gringrape <gringrape200@gmail.com>
2022-12-10 05:22:38 +0900

    상수풀 정보를 확인하기 위한 테스트 클래스 작성
    javac ${path}
    javap -v ${className}

---
+++ 20221210/src/Simple.java
@@ -0,0 +1,11 @@
+class Simple {
+    private final String greeting = "hello";
+
+    void main() {
+        int a = 111111;
+        int b = 222222;
+        System.out.println(a + b);
+        System.out.println("123");
+        System.out.println(greeting);
+    }
+}
