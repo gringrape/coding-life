be271f43fc435550b2cd29138cc4e8b78b25f23d
gringrape <gringrape200@gmail.com>
2023-01-03 01:23:37 +0900

    given 객체 정의 연습
    context parameter에 해당하는 객체를 삽입할 수 있도록
    given 정의를 활용하는 연습을 함.

--- 20221226/context-parameter/src/test/scala/Test.scala
+++ 20221226/context-parameter/src/test/scala/Test.scala
@@ -1,10 +1,26 @@
 import org.scalatest.funsuite.AnyFunSuite
 
+// 무엇을 할건가?
+// - Ordering 객체를 given definition 을 이용해서 구현한다. 
+// - 없애면 omit context paramter on calling 이 나온다. => 컴파일러가 타입에 맞는 객체를 발견하지 못한것. 
+
+trait Ordering[A]:
+  def lt(a: A, b: A): Boolean
+
+object Ordering:
+  given Int: Ordering[Int] with
+    def lt(a: Int, b: Int): Boolean = a < b
+  given String: Ordering[String] with
+    def lt(a: String, b: String): Boolean = a.length() < b.length()
+  given Double: Ordering[Double] with
+    def lt(a: Double, b: Double): Boolean = a < b
+
 class Test extends AnyFunSuite:
   def lessThan[A](a: A, b: A)(using ordering: Ordering[A]): Boolean =
     ordering.lt(a, b)
 
   test("Omit context parameter on calling") {
     assert(lessThan(1, 2) == true)
-    assert(lessThan(3, 2) == false)
+    assert(lessThan("999", "1111") == true)
+    assert(lessThan(1.1, 2.0) == true)
   }
