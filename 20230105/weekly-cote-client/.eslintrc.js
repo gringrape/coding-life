17e578247f9528cabf37553b0d918765e0b03f67
gringrape <gringrape200@gmail.com>
2023-01-05 20:32:06 +0900

    React 프로젝트 초기 설정

---
+++ 20230105/weekly-cote-client/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:react/jsx-runtime',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    // 'react/react-in-jsx-scope': 'off',
+  },
+};
---
+++ 20230105/weekly-cote-client/README.md
@@ -0,0 +1,11 @@
+# Weekly Cote Client
+```bash
+npm init -y
+npx eslint --init
+npm install jest @types/jest
+npm install -D parcel
+npm install react react-dom
+npm install --save-dev @testing-library/react
+npm install --save-dev @swc/core @swc/jest
+npm install --save-dev jest-environment-jsdom
+```
