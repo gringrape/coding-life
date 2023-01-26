ea45e5879665148b501b4e19c78d7f1fda19a8e8
gringrape <gringrape200@gmail.com>
2023-01-26 17:48:48 +0900

    Message queue 실험
    - Send message
    - Receive message

---
+++ 20230126/receive.py
@@ -0,0 +1,35 @@
+import pika
+import sys
+import os
+
+
+def main():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters('localhost')
+    )
+
+    channel = connection.channel()
+
+    channel.queue_declare(queue="hello")
+
+    def handle_message(ch, methos, properties, body):
+        print("Received: %r" % body)
+
+    channel.basic_consume(
+        queue="hello",
+        on_message_callback=handle_message,
+        auto_ack=True
+    )
+
+    print('Waiting for messages')
+    channel.start_consuming()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
