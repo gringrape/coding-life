1e73f5e0e93abcf24a772f31b2e509608bd3ae36
gringrape <gringrape200@gmail.com>
2023-01-27 12:13:52 +0900

    http4s POST 요청 실험

--- 20230127/http4s/build.sbt
+++ 20230127/http4s/build.sbt
@@ -5,5 +5,10 @@ libraryDependencies ++= Seq(
   "org.http4s" %% "http4s-ember-client" % "1.0.0-M37",
   "org.scalactic" %% "scalactic" % "3.2.15",
   "org.scalatest" %% "scalatest" % "3.2.15" % "test",
-  "org.http4s" %% "http4s-dsl" % http4sVersion
+  "org.http4s" %% "http4s-dsl" % "1.0.0-M37",
+  "org.http4s" %% "http4s-circe" % "1.0.0-M37",
+  // Optional for auto-derivation of JSON codecs
+  "io.circe" %% "circe-generic" % "0.14.2",
+  // Optional for string interpolation to JSON model
+  "io.circe" %% "circe-literal" % "0.14.2"
 )
--- 20230127/http4s/src/test/scala/Test.scala
+++ 20230127/http4s/src/test/scala/Test.scala
@@ -4,17 +4,32 @@ import cats.effect.IO
 
 import org.http4s.client.Client
 import org.http4s.client.JavaNetClientBuilder
-import org.http4s.syntax.all.uri
 import org.http4s.headers.Accept
 import org.http4s.Method
 import org.http4s.Request
 import org.http4s.MediaType
 import org.http4s.Headers
 
-import cats.effect.unsafe.implicits.global
+import org.http4s.implicits.uri
 
-class Test extends AnyFunSuite:
-  test("httpClient") {
+import org.http4s.client.dsl.io.http4sClientSyntaxMethod
+
+import cats.effect.unsafe.implicits.global
+import org.http4s.headers.`Content-Type`
+import org.http4s.Entity
+import org.http4s.UrlForm
+import org.http4s.EntityEncoder
+
+import cats.effect._
+import io.circe._
+import io.circe.literal._
+import org.http4s._
+import org.http4s.dsl.io._
+import org.http4s.implicits._
+import org.http4s.circe._
+
+class Test extends AnyFunSuite {
+  test("building HTTP GET request using Request API") {
     val httpClient: Client[IO] = JavaNetClientBuilder[IO].create
 
     val request = Request(
@@ -29,3 +44,22 @@ class Test extends AnyFunSuite:
 
     assert(response.contains("\"userId\":"))
   }
+
+  test("building HTTP POST request using Request API") {
+    val httpClient: Client[IO] = JavaNetClientBuilder[IO].create
+
+    val entityEncoder = EntityEncoder.encodeBy()
+
+    val request = Request(
+      method = Method.POST,
+      uri = uri"https://jsonplaceholder.typicode.com/posts",
+      headers = Headers(
+        `Content-Type`(MediaType.application.json)
+      )
+    ).withEntity(json"""{"name": "Alice"}""")
+
+    val response = httpClient.expect[String](request).unsafeRunSync()
+
+    assert(response.contains("\"id\":"))
+  }
+}
