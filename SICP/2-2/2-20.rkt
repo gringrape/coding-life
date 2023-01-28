d9eefc8a71ca26b53554d4a56288f3ce3e384934
gringrape <gringrape200@gmail.com>
2023-01-28 10:44:51 +0900

    함수 단위 사고를 이용한 풀이 개선

--- SICP/2-2/2-20.rkt
+++ SICP/2-2/2-20.rkt
@@ -4,9 +4,24 @@
 ; - 숫자들이 주어질때, 첫번째 숫자와 같은 parity를 가진 숫자들을 모은다.
 ; - dotted tail notation 을 활용한다.
 
+; ----------------------------------------------------
+; 풀이 1.
+; (define (same-parity first . rest)
+;   (define (hasSameParity? number)
+;     (= (remainder first 2) (remainder number 2)))
+
+;   (define (collect numbers)
+;     (if (null? numbers) nil
+;         (let ((head (car numbers)) (remaining (cdr numbers)))
+;           (if (hasSameParity? head)
+;               (cons head (collect remaining))
+;               (collect remaining)))))
+
+;   (cons first (collect rest)))
+
+; 풀이 2. 함수 단위 사고
 (define (same-parity first . rest)
-  (define (hasSameParity? number)
-    (= (remainder first 2) (remainder number 2)))
+  (define hasSameParity? (if (even? first) even? odd?))
 
   (define (collect numbers)
     (if (null? numbers) nil
@@ -19,6 +34,8 @@
 
 (same-parity 1 2 3 4 5 6 7) ; (1 3 5 7)
 (same-parity 2 3 4 5 6 7) ; (2 4 6)
+
+; ------------------------------------------------------
 
 ; 발생했던 문제.
 ; - nil에 대해 car, cdr 연산을 사용한 것이 문제.
