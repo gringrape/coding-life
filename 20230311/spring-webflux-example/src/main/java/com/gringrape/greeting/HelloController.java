cbc2a3ecf9c19a9b32c5d28d134405155202885f
gringrape <gringrape200@gmail.com>
2023-03-13 04:32:16 +0900

    실행결과를 종합하는 인터페이스 실험
    여러개의 실행결과를 나타내는 Flux를 생성하고, Mono를 통해 해당 결과를
    종합함.

--- 20230311/spring-webflux-example/src/main/java/com/gringrape/greeting/HelloController.java
+++ 20230311/spring-webflux-example/src/main/java/com/gringrape/greeting/HelloController.java
@@ -3,6 +3,7 @@ package com.gringrape.greeting;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -11,9 +12,19 @@ public class HelloController {
 
     @GetMapping
     public Mono<String> hello() {
-        return Mono.create(sink -> {
-            sink.success("Hello, World!");
-        });
+        return checkCode().all(i -> i)
+                .map(i -> i ? "Success" : "Failure");
     }
 
+    private Flux<Boolean> checkCode() {
+        return Flux.just(true, true, true)
+                .doOnNext(sink -> {
+                    try {
+                        Thread.sleep(300);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .onErrorReturn(false);
+    }
 }
