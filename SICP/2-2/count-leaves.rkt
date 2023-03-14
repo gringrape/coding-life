f3fe8517767d5f8ba3860919bd3ee9efc686d0cc
gringrape <gringrape200@gmail.com>
2023-03-15 00:48:23 +0900

    풀이 설명 추가

--- SICP/2-2/count-leaves.rkt
+++ SICP/2-2/count-leaves.rkt
@@ -2,11 +2,16 @@
 
 (#%require rackunit)
 
-(define (count-leaves tree)
-  (cond ((null? tree) 0)
-        ((not (pair? tree)) 1)
-        (else (+ (count-leaves (car tree))
-                 (count-leaves (cdr tree))))))
+; 구현 구상
+; - 현재 구조가 이파리인지 페어인지 판단.
+; - 페어면 재귀적으로 탐색.
+; - 이파리면 카운트 추가.
+
+(define (count-leaves node)
+  (cond ((null? node) 0)
+        ((not (pair? node)) 1)
+        (else (+ (count-leaves (car node))
+                 (count-leaves (cdr node))))))
 
 ; TEST
 
