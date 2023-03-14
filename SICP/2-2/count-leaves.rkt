05038caf7e13bd6774864323468aa89f2855c4a5
gringrape <gringrape200@gmail.com>
2023-03-15 00:41:13 +0900

    테스트 코드 추가

--- SICP/2-2/count-leaves.rkt
+++ SICP/2-2/count-leaves.rkt
@@ -2,13 +2,15 @@
 
 (#%require rackunit)
 
-(define x (cons (list 1 2) (list 3 4)))
-
 (define (count-leaves tree)
   (cond ((null? tree) 0)
         ((not (pair? tree)) 1)
         (else (+ (count-leaves (car tree))
                  (count-leaves (cdr tree))))))
+
+; TEST
+
+(define x (cons (list 1 2) (list 3 4)))
 
 (check-equal? (count-leaves x)
               4)
--- SICP/2-2/length.rkt
+++ SICP/2-2/length.rkt
@@ -1,17 +1,18 @@
 #lang sicp
 
-(define (assert-equals message a b)
-  (if (not (equal? a b))
-      (error message)))
+(#%require rackunit)
 
 (define (length tree)
   (if (null? tree)
       0
       (+ 1 (length (cdr tree)))))
 
-(assert-equals "트리의 길이"
-               (length(list 1 2 3 4 5))
-               5)
-(assert-equals "트리의 길이"
-               (length (cons (list 1 2) (list 3 4)))
-               3)
+; TEST
+
+(check-equal?
+ (length(list 1 2 3 4 5))
+ 5)
+
+(check-equal?
+ (length (cons (list 1 2) (list 3 4)))
+ 3)
