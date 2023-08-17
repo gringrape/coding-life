3066a4207c961db129d64b11bb63bc8ccbf61e45
gringrape <gringrape200@gmail.com>
2023-08-17 21:12:23 +0900

    SICP 범위에 있는 자연수 열거 프로시저

---
+++ SICP/2-2/accumulate.rkt
@@ -0,0 +1,23 @@
+#lang sicp
+
+(#%require rackunit)
+
+; Implementation
+(define (accumulate operation initial sequence)
+  (if (null? sequence)
+      initial
+      (+ (car sequence)
+         (accumulate operation initial (cdr sequence)))))
+
+; Test
+(check-equal? (accumulate + 0 nil) 0)
+
+(check-equal? (accumulate + 1 (list 2)) 3)
+
+(check-equal? (accumulate + 1 (list 2 4 5)) 12)
+
+; learned
+; - 가장 단순한 케이스를 작성하는 것에는 의미가 있다.
+; - 문제를 해결하는 보폭은 신경을 쓰지 않는다면 커진다.
+; - 재귀적 사고방식의 핵심은 이미 문제가 해결되었다고 가정할때, 작은 부분으로 쪼개는 것.
+;   (이 다음은 내 알바 아니다라는 태도가 중요함.)
---
+++ SICP/2-2/enumerate-interval.rkt
@@ -0,0 +1,23 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (enumerate-interval from to)
+  (if (> from to)
+      nil
+      (cons from (enumerate-interval (inc from) to))))
+
+; Test
+
+(check-equal? (enumerate-interval 2 1) nil)
+
+(check-equal? (enumerate-interval 2 2) (list 2))
+
+(check-equal? (enumerate-interval 2 3) (list 2 3))
+
+(check-equal? (enumerate-interval 1 5) (list 1 2 3 4 5))
+
+; learned
+
+; - CTRL+Z 를 봉인하자.
+; - 잘못했을때 처음 부터 다시 만들게 되니, 자연스렙게 차분해졌다.
