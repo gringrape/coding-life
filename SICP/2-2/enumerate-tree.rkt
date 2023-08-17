2465a8f7ac39c8b59eafe93bb23136953ea819d8
gringrape <gringrape200@gmail.com>
2023-08-18 06:45:53 +0900

    SICP, tree 원소 열거 프로시저

---
+++ SICP/2-2/enumerate-tree.rkt
@@ -0,0 +1,23 @@
+#lang sicp
+
+(#%require rackunit)
+
+; Procedure
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        ((pair? tree) (append (enumerate-tree (car tree))
+                              (enumerate-tree (cdr tree))))))
+
+; Test
+(check-equal? (enumerate-tree nil) nil)
+
+(check-equal? (enumerate-tree (list 1)) (list 1))
+
+(check-equal? (append (list 1) (list 2 3)) (list 1 2 3))
+
+(check-equal? (enumerate-tree (list (list 1 2) 3)) (list 1 2 3))
+
+(check-equal? (enumerate-tree (list 1 (list 2 3) 4)) (list 1 2 3 4))
+
+(check-equal? (enumerate-tree (list (list 1 2) (list 3 4) 5)) (list 1 2 3 4 5))
