9379a326d9fcf5b74136b848df466bc1e023ff1f
gringrape <gringrape200@gmail.com>
2023-12-18 03:31:58 +0900

    오류 수정

--- SICP/2-2/2-44.rkt
+++ SICP/2-2/2-44.rkt
@@ -1,7 +1,11 @@
 #lang sicp
 
+; dummy
+(define (below f g) f)
+(define (beside f g) f)
+
 (define (up-split painter n)
   (if (= n 0)
-    painter
-    (let ((smaller (up-split painter (- n 1))))
+      painter
+      (let ((smaller (up-split painter (- n 1))))
         (below painter (beside smaller smaller)))))
--- SICP/2-2/2-45.rkt
+++ SICP/2-2/2-45.rkt
@@ -3,6 +3,6 @@
 (define (split f g)
   (lambda (painter n)
     (if (= n 0)
-      painter
-      (let (smaller ((split f g) painter (- n 1))) 
-        (f (g smaller smaller))))))
+        painter
+        (let ((smaller ((split f g) painter (- n 1))))
+          (f (g smaller smaller))))))
