2d7e9b7a181734b9bb14ce7614ac4c17c2bc2654
gringrape <gringrape200@gmail.com>
2024-01-17 07:23:38 +0900

    팩토리 메서드

--- 20240104/tdd/app/src/main/java/tdd/Money.java
+++ 20240104/tdd/app/src/main/java/tdd/Money.java
@@ -1,11 +1,23 @@
 package tdd;
 
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
 
     @Override
     public boolean equals(Object object) {
+        // TODO: currency 개념 도입
         Money money = (Money) object;
-        return amount == money.amount;
+        return amount == money.amount
+                && getClass().equals(object.getClass());
     }
+
+    abstract public Money times(int multiplier);
 }
